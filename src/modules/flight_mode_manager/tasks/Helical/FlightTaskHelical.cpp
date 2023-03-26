5557d3dabecfec22c44400458c5873d2aed818f6
Stefano <stefano2.colli@mail.polimi.it>
2023-03-26 15:28:11 +0200

    Added topic Added helical trajectory topic to track its status

--- src/modules/flight_mode_manager/tasks/Helical/FlightTaskHelical.cpp
+++ src/modules/flight_mode_manager/tasks/Helical/FlightTaskHelical.cpp
@@ -7,14 +7,22 @@
  */
 bool FlightTaskHelical::activate(const vehicle_local_position_setpoint_s &last_setpoint)
 {
+	_helical_trajectory_publisher.advertise();
+
 	bool ret = FlightTask::activate(last_setpoint);
 
-	_starting_pos = _position; //the curent position is the starting point of the helix
+	//the starting point of the helix is at _helix_radius distance (along x) from the current position
+	_starting_pos(0) = _position(0) + _helix_radius;
+	_starting_pos(1) = _position(1);
+	_starting_pos(2) = _position(2);
+
+	//the helix origin is the current position when we call the task
+	_helix_origin = _position;
 
 	//calculate the finish time to perform the requested number of loops
-	_finish_time = (float) _loops * (2 * (float) MATH_PI / _trajectory_speed); //each loop takes 2*pi/speed seconds
+	_trajectory_duration = (float) _loops * (2 * (float) MATH_PI / _trajectory_speed); //each loop takes 2*pi/speed seconds
 
-	PX4_INFO("Initiating Helical trajectory");
+	//PX4_INFO("Initiating Helical trajectory");
 	return ret;
 }
 
@@ -29,21 +37,58 @@ bool FlightTaskHelical::activate(const vehicle_local_position_setpoint_s &last_s
  */
 bool FlightTaskHelical::update()
 {
-	//compute time (in seconds) elapsed from the starting of the trajectory
-	float time = (_time_stamp_current - _time_stamp_activate) / 1e6f; //timestamps are in ms
+	helical_trajectory_s msg{};
+	msg.timestamp = hrt_absolute_time();
 
-	if (time <= _finish_time)
+	if (((_position - _starting_pos).longerThan(_tolerance_radius) ||
+		_velocity.longerThan(_tolerance_vel)) &&
+		_time_stamp_start_helix == 0)
 	{
-		_position_setpoint(0) = _starting_pos(0) + _helix_radius * cosf(time*_trajectory_speed);
-		_position_setpoint(1) = _starting_pos(1) + _helix_radius * sinf(time*_trajectory_speed);
-		_position_setpoint(2) = _starting_pos(2) - _vertical_separation * time*_trajectory_speed; //NED frame
+		//we still are too far from the starting position, or too fast
+		_position_setpoint = _starting_pos;
+
+		//PX4_INFO("Approaching starting position");
+		msg.started = false;
+		msg.ended = false;
 	}
 	else
 	{
-		//end holding reached position
-		_position_setpoint = _position;
-		PX4_INFO("Ended Helical trajectory of %d loops", _loops);
+		//we are in the neighborhood of the starting position
+
+		_velocity_setpoint.setNaN();
+
+		//set the timestamp at which the actual trajectory starts
+		if (_time_stamp_start_helix == 0)
+		{
+			_time_stamp_start_helix = hrt_absolute_time();
+		}
+
+		//compute time (in seconds) elapsed from the starting of the trajectory
+		float trajectory_time = (_time_stamp_current - _time_stamp_start_helix) / 1e6f; //timestamps are in ms
+
+		//helical trajectory
+		if (trajectory_time <= _trajectory_duration)
+		{
+			_position_setpoint(0) = _helix_origin(0) + _helix_radius * cosf(trajectory_time*_trajectory_speed);
+			_position_setpoint(1) = _helix_origin(1) + _helix_radius * sinf(trajectory_time*_trajectory_speed);
+			_position_setpoint(2) = _helix_origin(2) - _vertical_separation * trajectory_time*_trajectory_speed; //NED frame
+
+			//PX4_INFO("Following helical trajectory");
+			msg.started = true;
+			msg.ended = false;
+		}
+		else
+		{
+			//end holding reached position
+			//_position_setpoint = _position;
+
+
+			//PX4_INFO("Helical trajectory terminated");
+			msg.started = true;
+			msg.ended = true;
+		}
 	}
 
+	_helical_trajectory_publisher.publish(msg);
 	return true;
 }
--- src/modules/flight_mode_manager/tasks/Helical/FlightTaskHelical.hpp
+++ src/modules/flight_mode_manager/tasks/Helical/FlightTaskHelical.hpp
@@ -2,6 +2,8 @@
 
 #include "FlightTask.hpp"
 #include <matrix/matrix/math.hpp>
+#include <uORB/topics/helical_trajectory.h>
+#include <uORB/Publication.hpp>
 
 class FlightTaskHelical : public FlightTask
 {
@@ -14,10 +16,15 @@ public:
 	bool activate(const vehicle_local_position_setpoint_s &last_setpoint) override;
 
 private:
-	matrix::Vector3f _starting_pos{ 0.f, 0.f, 0.f}; //starting z coord of the helix
+	matrix::Vector3f _starting_pos{ 0.f, 0.f, 0.f}; //starting position coord of the helix
+	matrix::Vector3f _helix_origin{0.f, 0.f, 0.f}; //origin of the helical trajectory
 	static constexpr float _helix_radius = 5.f; //radius of the helix (in meters)
 	static constexpr float _vertical_separation = 1.f; //vertical separation between a coil and another (in meters)
-	static constexpr float _trajectory_speed = 0.5f; //factor to scale the speed of the trajectory
+	static constexpr float _trajectory_speed = 0.4f; //factor to scale the speed of the trajectory
 	static constexpr uint _loops = 3; //number of loops in the helical trajectory
-	float _finish_time;
+	static constexpr float _tolerance_radius = 0.1f; //tolerance radius for the starting point
+	static constexpr float _tolerance_vel = 0.1f; //tolerance velocity for starting the helical trajectory
+	hrt_abstime _time_stamp_start_helix = 0; //timestamp at which the actual helical trajectory started (not the flight task)
+	float _trajectory_duration; //estimated time duration of the helical trajectory
+	uORB::Publication<helical_trajectory_s> _helical_trajectory_publisher{ORB_ID(helical_trajectory)}; //handle to publish trajectory status
 };
